146de25e6a87f96210dd5fb456615dcb2cfe1a66
Matthias Grob <maetugr@gmail.com>
2023-04-28 10:57:02 +0200

    ubuntu.sh: replace gazebo-garden with gazebo-classic

--- Tools/setup/ubuntu.sh
+++ Tools/setup/ubuntu.sh
@@ -229,7 +229,7 @@ if [[ $INSTALL_SIM == "true" ]]; then
 		sudo apt-get update -y --quiet
 
 		# Install Gazebo
-		gazebo_packages="gz-garden"
+		gazebo_packages="gazebo libgazebo-dev"
 	else
 		sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
 		wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
